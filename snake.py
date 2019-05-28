b158f5fdd1b18b34224b57e056a48bfe27956d10
Macrae <40277542@live.napier.ac.uk>
2019-05-28 15:35:37 +0100

    Pickups and user moving works

--- snake.py
+++ snake.py
@@ -19,37 +19,35 @@ def Init():
 
 
 head = Init()
-print(head)
 
 
 def Render():
     for x in range(10):
         for y in range(10):
             if not board[x][y].isdigit():
-                print(board[x][y], end="")
+                print(" "+board[x][y]+" ", end = "")
             else:
-                print("_", end="")
+                print(" _ ", end = "")
         print("")
 
 
-def Move(head):
+def Move(head, direction):
     if direction == "a":
-        head = (head[0], head[1] -1)
+        head = (head[0], head[1] - 1)
         if head[1] < 0:
             head = (head[0], 9)
-
     if direction == "w":
         head = (head[0] -1, head[1])
         if head[0] < 0:
             head = (9, head[1])
 
     if direction == "d":
-        head = (head[0], head[1] +1)
+        head = (head[0], head[1] + 1)
         if head[1] > 9:
             head = (head[0], 0)
 
     if direction == "s":
-        head = (head[0] +1, head[1])
+        head = (head[0] + 1, head[1])
         if head[0] > 9:
             head = (0, head[1])
     return head
@@ -62,17 +60,31 @@ def Update(head):
     Render()
     return head
 
+def UserUpdate(head):
+    direction = input("enter: ")
+    while direction != "q":
+        board[head[0]][head[1]] = str(head[1])
+        head = Move(head,direction)
+        board[head[0]][head[1]] = "h"
+        Render()
+        direction = input("enter: ")
+    return head
+
+
 def SpawnPickup(snake):
     tiles = []
     for x in range(10):
         for y in range(10):
-            if board[x][y] != 'h':
-                temp = (x + 1, y)
+            if board[x][y] != 'h' and board[x][y] != 'p':
+                temp = (x, y)
                 tiles.append(temp)
-    print(tiles)
+    pickup = random.randint(0, len(tiles))
+    board[tiles[pickup][0]][tiles[pickup][1]] = "p"
 
 
-
-
-head = Update(head)
 SpawnPickup(snake)
+SpawnPickup(snake)
+Render()
+head = UserUpdate(head)
+
+print(head)
